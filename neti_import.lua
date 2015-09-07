3065acbe42dc3e15e69197424a0191998ee11b97
Markus Breit <markus.breit@gcsc.uni-frankfurt.de>
2015-09-07 13:41:19 +0000

    cable: NETI import script

---
+++ neti_import.lua
@@ -0,0 +1,16 @@
+--------------------------------------------------------------
+-- This script will create a .ugx file from NeuGen output	--
+--------------------------------------------------------------
+
+ug_load_script("ug_util.lua")
+
+-- get a geometry importer provider
+gip = NeuronalTopologyImporterProvider()
+
+-- get the default geometry importer
+gi = gip:getDefaultNeuronalTopologyImporter()
+
+-- base name
+baseName = util.GetParam("-name", "testNetwork")
+-- import
+gi:import_txt(baseName.."_secs.txt", baseName.."_connex.txt", baseName.."_synapses.txt")
