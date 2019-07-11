686d2eb26452c8460e88dc6d43fb6603eb9c41d6
mbreit <markus.breit@gcsc.uni-frankfurt.de>
2019-07-11 17:07:00 +0200

    added optional joining criteria in neti_import.lua

--- neti_import.lua
+++ neti_import.lua
@@ -17,10 +17,32 @@ baseName = util.GetParam("-name", "testNetwork")
 method = util.GetParam("-method", "txt")
 
 if method == "hoc" or method == "ngx" then
+	--[[
+	-- for import of bAP cells
+	--neti:add_joining_criterion("Rad_thick_prox")
+	--neti:add_joining_criterion("Rad_thick_med")
+	--neti:add_joining_criterion("Rad_thick_dist")
+	--neti:add_joining_criterion("Rad_thin")
+	neti:add_joining_criterion("Rad")
+	--neti:add_joining_criterion("L_M_thick")
+	--neti:add_joining_criterion("L_M_med")
+	--neti:add_joining_criterion("L_M_thin")
+	neti:add_joining_criterion("L_M")
+	neti:add_joining_criterion("soma")
+	--neti:add_joining_criterion("basal_prox")
+	--neti:add_joining_criterion("basal_dist")
+	neti:add_joining_criterion("basal")
+	neti:add_joining_criterion("hill")
+	neti:add_joining_criterion("iseg")
+	neti:add_joining_criterion("inode")
+	--neti:add_joining_criterion("node")
+	--]]
+	---[[
 	neti:add_joining_criterion("soma")
 	neti:add_joining_criterion("dend")
 	neti:add_joining_criterion("apic")
 	neti:add_joining_criterion("axon")
+	--]]
 end
 
 neti:import_geometry_and_generate_grid(baseName, method)
