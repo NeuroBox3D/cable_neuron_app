8c616bab6e67cddd05ba9946dfd5b6b262c1a0dd
mbreit <markus.breit@gcsc.uni-frankfurt.de>
2019-09-25 12:31:17 +0200

    added header

--- conversion_ugx2swc.lua
+++ conversion_ugx2swc.lua
@@ -1,12 +1,19 @@
---------------------------------------------------------------
--- This script will create a .ugx file from NeuGen output	--
---------------------------------------------------------------
+--------------------------------------------------------------------------------
+-- This script will extract one cell from a NeuGen-created network.	          --
+-- It is chosen to be the innermost cell of a specific subset.                --
+-- The cell is then exported as separate SWC file, retaining all diameter,    --
+-- but losing all synapse information.                                        --
+--                                                                            --
+--  Author:  mbreit                                                           --
+--  Date:    2017-03-25                                                       --
+--------------------------------------------------------------------------------
 
 ug_load_script("ug_util.lua")
+AssertPluginsLoaded({"cable_neuron"})
 
-ugxName = util.GetParam("-grid", "testNetwork_splitSyn.ugx")
+ugxName = util.GetParam("-grid", "cable_neuron_app/grids/testNetwork.ugx")
 
-InitUG(3, AlgebraType("CPU", 1));
+InitUG(3, AlgebraType("CPU", 1))
 dom1d = util.CreateDomain(ugxName, 0)
 
 nid = innermost_neuron_id_in_subset("SOMA__L5A_PYRAMIDAL", dom1d:subset_handler())
